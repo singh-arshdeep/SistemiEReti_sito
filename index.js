bb89326a8bf39eaaa108c1b1b47fbb876f65474d
singh-arshdeep <singh.arshdeep1204@gmail.com>
2026-04-09 20:21:28 +0200

    1.1.12.1.1.11.11.1.1.1.1.1.\1
    modifica modifica

---
+++ index.js
@@ -0,0 +1,41 @@
+require('dotenv').config();
+
+/* Server Node */
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 8000;
+
+const path = require('path')
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'))
+})
+
+app.listen(port, () => {
+    console.log(`Server node in esecuzione sulla porta ${port}`)
+})
+
+app.post('/invio', (req, res) => {
+    const { garage } = req.body;
+    if (!garage || (garage != "aperto" && garage != "chiuso")) {
+        console.log("Dati Errati")
+    } else if (garage == "aperto") {
+        res.json({
+            garage: "aperto"
+        })
+        console.log(JSON.stringify({
+            garage: "aperto"
+        }))
+    } else if (garage == "chiuso") {
+        res.json({
+            garage: "chiuso"
+        })
+        console.log(JSON.stringify({
+            garage: "chiuso"
+        }))
+    }
+})
